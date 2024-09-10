719797e64bad517cd519d251a363ee63b744ead7
Kamiya Gaikwad <kamitarg88@gmail.com>
2024-09-11 03:49:24 +0530

    Add function for location check

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('searchBtn').addEventListener('click', function() {
-      const city = document.getElementById('city').value;
-      if (city) {
-        console.log('City detected as', city);
-        // Will add API call here later
-      }
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('searchBtn').addEventListener('click', function () {
+        const city = document.getElementById('city').value;
+        if (city) {
+            console.log('City detected as', city);
+            // Will add API call here later
+        }
+    });
+
+    document.getElementById('locationBtn').addEventListener('click', function () {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+                console.log('Searching for location using device:', latitude, longitude);
+                // TODO: Add api call here
+            }, function (error) {
+                console.error('Error getting location:', error);
+            });
+        } else {
+            alert('Geolocation is not supported by this browser.');
+        }
     });
 
 });
